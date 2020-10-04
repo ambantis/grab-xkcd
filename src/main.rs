7a29048297e8e0a9ce9d30a5728fb6e34ffcc191
Alexandros Bantis <ambantis@gmail.com>
2020-10-04 06:25:56 -0700

    Deserialize HTTP response

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,7 @@
+use anyhow::Result;
 use clap::Clap;
+use serde::Deserialize;
+use std::convert::TryFrom;
 
 /// A utility to grab XKCD comics
 #[derive(Clap)]
@@ -34,10 +37,57 @@ impl XkcdClient {
     fn new(args: Args) -> Self {
         XkcdClient { args }
     }
+
+    fn run(&self) -> Result<()> {
+        Ok(())
+    }
 }
 
-fn main() {
+#[derive(Deserialize, Debug)]
+struct ComicResponse {
+    month: String,
+    num: usize,
+    link: String,
+    year: String,
+    news: String,
+    safe_title: String,
+    transcript: String,
+    alt: String,
+    img: String,
+    title: String,
+    day: String,
+}
+
+impl TryFrom<String> for ComicResponse {
+    type Error = anyhow::Error;
+
+    fn try_from(json: String) -> Result<Self, Self::Error> {
+        serde_json::from_str(&json).map_err(|e| e.into())
+    }
+}
+
+struct Comic {
+    title: String,
+    num: usize,
+    date: String,
+    desc: String,
+    img_url: String,
+}
+
+impl From<ComicResponse> for Comic {
+    fn from(cr: ComicResponse) -> Self {
+        Comic {
+            title: cr.title,
+            num: cr.num,
+            date: format!("{}-{}-{}", cr.day, cr.month, cr.year),
+            desc: cr.alt,
+            img_url: cr.img,
+        }
+    }
+}
+
+fn main() -> Result<()> {
     let args = Args::parse();
     let client = XkcdClient::new(args);
-    // ... todo
+    client.run()
 }
